f7b22d7aa27b18811b96492cc5e6952e27d1d6b2
pythonicforge <pythonicforge@gmail.com>
2024-12-28 09:57:54 +0530

    del: CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
+import "./assets/styles/main.scss";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
